834ff966281e4c9c84b98a9405e0490dd2ee6929
pintchom <maxpintchouk4321@gmail.com>
2023-07-13 16:57:54 -0400

    Initial Commit

---
+++ YouAreAwesomeApp/YouAreAwesomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeAppApp.swift
+//  YouAreAwesomeApp
+//
+//  Created by Max Pintchouk on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
